ce1472cdc25137768c6a582cbdd5e94508b724b1
doeg <855595+doeg@users.noreply.github.com>
2022-12-10 15:15:47 -0500

    kk

--- 2022-09/main.go
+++ 2022-09/main.go
@@ -14,24 +14,31 @@ func main() {
 }
 
 func partOne(input []string) {
+	visited := make(map[string]bool)
+
 	h := &util.Coords{X: 0, Y: 0}
+	t := &util.Coords{X: 0, Y: 0}
 
 	for _, line := range input {
 		dir, mag := parseInstruction(line)
 		fmt.Printf("==== %s%d ====\n", dir, mag)
-		printGrid(h)
+		printGrid(h, t)
 
 		hs := make([]string, 0)
 		hs = append(hs, h.ToString())
 
+		ts := make([]string, 0)
+		ts = append(ts, t.ToString())
+
 		for i := 0; i < mag; i++ {
+			// Move H
 			switch dir {
 			case "U":
-				h.Y--
+				h.Y++
 			case "R":
 				h.X++
 			case "D":
-				h.Y++
+				h.Y--
 			case "L":
 				h.X--
 			default:
@@ -40,13 +47,35 @@ func partOne(input []string) {
 
 			hs = append(hs, h.ToString())
 
-			printGrid(h)
+			// Move T
+			if h.IsAdjacent(t) {
+				ts = append(ts, t.ToString())
+			} else {
+				if h.X > t.X {
+					t.X++
+				}
+				if h.X < t.X {
+					t.X--
+				}
+				if h.Y > t.Y {
+					t.Y++
+				}
+				if h.Y < t.Y {
+					t.Y--
+				}
+			}
+
+			visited[t.ToString()] = true
+			printGrid(h, t)
 		}
 
 		fmt.Printf("H: %s\n", strings.Join(hs, " -> "))
+		fmt.Printf("T: %s\n", strings.Join(ts, " -> "))
 		fmt.Println()
-
 	}
+
+	fmt.Printf("%+v\n", visited)
+	fmt.Println(len(visited))
 }
 
 func parseInstruction(line string) (string, int) {
@@ -55,9 +84,14 @@ func parseInstruction(line string) (string, int) {
 	return s[0], mag
 }
 
-func printGrid(h *util.Coords) {
+func printGrid(h, t *util.Coords) {
 	size := 11
 	hp, err := h.ToGridPosition(size)
+	if err != nil {
+		panic(err)
+	}
+
+	tp, err := t.ToGridPosition(size)
 	if err != nil {
 		panic(err)
 	}
@@ -68,6 +102,8 @@ func printGrid(h *util.Coords) {
 			switch {
 			case hp.Row == row && hp.Col == col:
 				s = append(s, "H")
+			case tp.Row == row && tp.Col == col:
+				s = append(s, "T")
 			default:
 				s = append(s, ".")
 			}
