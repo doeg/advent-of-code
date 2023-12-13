dc8529b608963cf68b66789a354af46df88ff6d3
Sara Bee <855595+doeg@users.noreply.github.com>
2023-12-13 09:43:40 -0500

    Vertical index

--- 2023/2023-13-1.ts
+++ 2023/2023-13-1.ts
@@ -26,8 +26,49 @@ const parsePatterns = () => {
   return patterns;
 };
 
+const areMirrorImages = (left: string, right: string): boolean => {
+  const leftReversed = left.split("").reverse().join("");
+  const smallestLength = Math.min(left.length, right.length);
+  const leftSubstring = leftReversed.substring(0, smallestLength);
+  const rightSubstring = right.substring(0, smallestLength);
+  return leftSubstring === rightSubstring;
+};
+
+const getVerticalIndex = (pattern: Pattern): number | null => {
+  // Start at index 1, since a reflection over index 0 doesn't really make sense.
+  let col = 1;
+
+  while (col < pattern[0].length - 1) {
+    let allRowsMirroredAtIndex = true;
+
+    for (let row = 0; row < pattern.length; row++) {
+      const line = pattern[row];
+
+      const first = line.substring(0, col);
+      const second = line.substring(col);
+      const isMirrored = areMirrorImages(first, second);
+
+      if (!isMirrored) {
+        allRowsMirroredAtIndex = false;
+        break;
+      }
+    }
+
+    if (allRowsMirroredAtIndex) {
+      return col;
+    }
+
+    col++;
+  }
+
+  return null;
+};
+
 const patterns = parsePatterns();
-patterns.forEach((pattern) => {
-  console.log(pattern);
-});
-console.log(patterns.length);
+console.log(getVerticalIndex(patterns[0]));
+console.log(getVerticalIndex(patterns[1]));
+
+// patterns.forEach((pattern) => {
+//   console.log(pattern);
+// });
+// console.log(patterns.length);
