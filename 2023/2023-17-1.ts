f14d5fb0d8571ad599465f369109afde21a3e452
doeg <855595+doeg@users.noreply.github.com>
2023-12-17 08:57:49 -0500

    nodes

--- 2023/2023-17-1.ts
+++ 2023/2023-17-1.ts
@@ -3,22 +3,48 @@ import chalk from "chalk";
 
 const USE_EXAMPLE = true;
 
-const parseGrid = (): number[][] => {
+interface Position {
+  row: number;
+  col: number;
+}
+
+interface Node {
+  position: Position;
+  id: string;
+  value: number;
+}
+
+const toKey = ({ row, col }: Position): string => `${row}__${col}`;
+
+const fromKey = (key: string): Position => {
+  const [row, col] = key.split("__");
+  return { row: parseInt(row), col: parseInt(col) };
+};
+
+const parseGrid = (): Node[][] => {
   const input = getInput(__filename, USE_EXAMPLE).split("\n");
-  const grid: number[][] = [];
+  const grid: Node[][] = [];
 
   for (let row = 0; row < input.length; row++) {
     if (!input[row]) continue;
-    const line = input[row].split("").map((i) => parseInt(i));
-    grid[row] = line;
+    const line = input[row].split("");
+    const nodes: Node[] = line.map((s, col): Node => {
+      return {
+        position: { row, col },
+        id: toKey({ row, col }),
+        value: parseInt(s),
+      };
+    });
+
+    grid[row] = nodes;
   }
 
   return grid;
 };
 
-const printGrid = (grid: number[][]) => {
+const printGrid = (grid: Node[][]) => {
   for (let row = 0; row < grid.length; row++) {
-    console.log(grid[row].join(" "));
+    console.log(grid[row].map((n) => n.value).join(" "));
   }
 };
 
