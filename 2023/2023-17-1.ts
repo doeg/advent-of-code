3ca300ff86627ea920f5c56a4b6b0d3666721473
Sara Bee <855595+doeg@users.noreply.github.com>
2023-12-17 15:53:49 -0500

    Fuck it off

--- 2023/2023-17-1.ts
+++ 2023/2023-17-1.ts
@@ -91,6 +91,62 @@ const getCheapestPathIndex = (grid: number[][], paths: Path[]): number => {
   return minPathIndex;
 };
 
+const getNodesUp = (
+  grid: number[][],
+  node: Position,
+  MAX_NODES: number = 3
+): Position[] => {
+  let nodes: Position[] = [];
+  let i = 1;
+  while (i < MAX_NODES && node.row - i >= 0) {
+    nodes.push({ row: node.row - i, col: node.col });
+    i++;
+  }
+  return nodes;
+};
+
+const getNodesDown = (
+  grid: number[][],
+  node: Position,
+  MAX_NODES: number = 3
+): Position[] => {
+  let nodes: Position[] = [];
+  let i = 1;
+  while (i < MAX_NODES && node.row + i < grid.length) {
+    nodes.push({ row: node.row + i, col: node.col });
+    i++;
+  }
+  return nodes;
+};
+
+const getNodesLeft = (
+  grid: number[][],
+  node: Position,
+  MAX_NODES: number = 3
+): Position[] => {
+  let nodes: Position[] = [];
+  let i = 1;
+  while (i < MAX_NODES && node.col - i >= 0) {
+    nodes.push({ row: node.row, col: node.col - i });
+    i++;
+  }
+  return nodes;
+};
+
+const getNodesRight = (
+  grid: number[][],
+  node: Position,
+  MAX_NODES: number = 3
+): Position[] => {
+  let nodes: Position[] = [];
+  let i = 1;
+  while (i < MAX_NODES && node.col + i < grid[0].length) {
+    nodes.push({ row: node.row, col: node.col + i });
+    i++;
+  }
+  return nodes;
+};
+
 // Returns up to `MAX_NODES` next nodes from (but not including) `node` in the given `direction`.
 // Will return fewer than `MAX_NODES` if any of them are out of bounds of `grid`.
 const getNextNodes = (
@@ -99,20 +155,16 @@ const getNextNodes = (
   direction: Direction,
   MAX_NODES: number = 3
 ): Position[] => {
-  let nodes: Position[] = [];
   switch (direction) {
     case "up":
+      return getNodesUp(grid, node, MAX_NODES);
     case "down":
+      return getNodesDown(grid, node, MAX_NODES);
     case "left":
-      return [];
+      return getNodesLeft(grid, node, MAX_NODES);
     case "right":
-      let i = 1;
-      while (i <= MAX_NODES && node.col + i < grid[0].length) {
-        nodes.push({ row: node.row, col: node.col + i });
-        i++;
-      }
+      return getNodesRight(grid, node, MAX_NODES);
   }
-  return nodes;
 };
 
 const partOne = () => {
@@ -131,56 +183,71 @@ const partOne = () => {
     {
       nodes: [initialNode],
       direction: "right",
+    },
+    {
+      nodes: [initialNode],
+      direction: "down",
     }
-    // FIXME
-    // {
-    //   nodes: [initialNode],
-    //   direction: "down",
-    // }
   );
 
   let step = 0;
 
   while (openPaths.length > 0) {
     step++;
-    console.log("\n\n=======================");
-    console.log("ITERATION", step);
+
+    // console.log("\n\n=======================");
+    // console.log("ITERATION", step);
 
     const pathIndex = getCheapestPathIndex(grid, openPaths);
     const path = openPaths.splice(pathIndex, 1)[0];
 
-    debugger;
-
     const lastNodeInPath = path.nodes[path.nodes.length - 1];
-    console.log("last node in path:", lastNodeInPath);
+    // console.log("last node in path:", lastNodeInPath);
+
+    if (
+      lastNodeInPath.row === targetNode.row &&
+      lastNodeInPath.col === targetNode.col
+    ) {
+      console.log("DONE!!!!!!!!");
+      printGrid(grid, path);
+      return;
+    }
 
     if (lastNodeInPath.row < 0) continue;
     if (lastNodeInPath.row >= grid.length) continue;
     if (lastNodeInPath.col < 0) continue;
     if (lastNodeInPath.col >= grid[0].length) continue;
 
-    console.log(path);
+    // console.log(path);
     printGrid(grid, path);
 
+    // The next nodes in the path given the current path direction.
     const nextNodesInPath = getNextNodes(grid, lastNodeInPath, path.direction);
+
+    const nextDirections = NEXT_DIRECTIONS[path.direction];
 
     let subNodes: Position[] = [];
     nextNodesInPath.forEach((node) => {
       subNodes.push(node);
       const nextNodes = [...path.nodes, ...subNodes];
-      console.log(nextNodes);
-      openPaths.push({
-        nodes: nextNodes,
-        direction: path.direction, // FIXME this should be the next direction
+
+      nextDirections.forEach((direction) => {
+        openPaths.push({
+          nodes: nextNodes,
+          direction,
+        });
       });
     });
 
-    console.log("next nodes in path");
-    console.log(JSON.stringify(nextNodesInPath, null, 2));
+    // console.log("next nodes in path");
+    // console.log(JSON.stringify(nextNodesInPath, null, 2));
 
-    console.log("remaining paths:", openPaths.length);
-    console.log();
+    // console.log("remaining paths:", openPaths.length);
+    // console.log();
+
+    // debugger;
   }
+  console.log(step);
 };
 
 partOne();
